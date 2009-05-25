58a7d6b49d00cb53da345e6a045a1a7a553ca8ca
Keller Davis <kellerdavis@kellermini.(none)>
2009-05-25 12:27:43 -0400

    think i finished sample ruby app

--- nqueens.rb
+++ nqueens.rb
@@ -1,26 +1,41 @@
 #!/opt/local/bin/ruby1.9
 class ChessBoard
+  attr_reader :queens
   def initialize(size)
     @size = size
+    @queens = []
   end
   
-  def add_queen(column,queens)
-    if column>@size
-      p queens
-      return true
-    end
-    return true if column>@size
+  def add_queen
+    column = @queens.count
+    return @queens if column==@size
     for row in 1..@size
-      next if queens.include?(row)
+      next if @queens.include?(row)
       #detect diags
-      next if queens.detect{|i|row-column+queens.index(i) == i or row+column-queens.index(i) == i} != nil
-      queens[column]=row;
-      next if add_queen(column+1,queens) == false
+      next if @queens.detect{|i|row-column+@queens.index(i) == i or row+column-@queens.index(i) == i} != nil
+      @queens[column]=row;
+      
+      if add_queen == false
+        @queens.delete_at(column)
+        next
+      else
+        return true
+      end
     end
     return false
   end
   
+  def print_board
+    puts '-'*(2*@size)+'-'
+    for i in 1..@size
+      column = @queens.index(i)
+      puts '| '*column+'|x'+'| '*(@size-column)
+    end
+    puts '-'*(2*@size)+'-'
+  end
 end
 
 board = ChessBoard.new(8)
-p board.add_queen(0,[])
+board.add_queen
+board.print_board
+#p board.queens
